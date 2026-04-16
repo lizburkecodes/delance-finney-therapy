a47c839807a3238fa5f1bc7c898a31b9620519c6
Liz Burke <liz.burke.code@gmail.com>
2026-04-16 16:57:49 -0400

    placeholder page

--- src/pages/Training.jsx
+++ src/pages/Training.jsx
@@ -1,7 +1,20 @@
+import '../styles/training.css';
+
 function Training() {
-    return (
-        <h1>Training</h1>
-    );
+  return (
+    <section className="placeholder-page">
+      <div className="placeholder-content">
+        <p className="section-label">Training</p>
+
+        <h1>Page in progress</h1>
+
+        <p>
+          This section is currently being developed. More information about my
+          training and experience will be added soon.
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default Training;
