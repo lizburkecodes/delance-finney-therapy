3c062c72bd7a8f424435e19b589eb63731e98b60
Liz Burke <liz.burke.code@gmail.com>
2026-04-16 11:56:23 -0400

    wire up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { Routes, Route } from 'react-router-dom';
+import Layout from './layout/Layout';
+
+import Home from './pages/Home';
+import Community from './pages/Community';
+import Training from './pages/Training';
+import Contact from './pages/Contact';  
 
 function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<h1>Website à la Delancey!</h1>} />
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="community" element={<Community />} />
+        <Route path="training" element={<Training />} />
+        <Route path="contact" element={<Contact />} />
+      </Route>
     </Routes>
   )
 }
